939e5b73c1c962ee4219dc4ac843be277273e67c
Nathã Emanuel <92585791+NathaEmanuel17@users.noreply.github.com>
2026-05-16 21:14:14 -0300

    Create CRUD department

--- src/mode/dao/DaoFactory.java
+++ src/mode/dao/DaoFactory.java
@@ -2,10 +2,15 @@ package mode.dao;
 
 import db.DB;
 import model.dao.impl.SellerDaoJDBC;
+import model.dao.impl.DepartamentDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartamentDaoJDBC(DB.getConnection());
 	}
 }
